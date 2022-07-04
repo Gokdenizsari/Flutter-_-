11afe6fc2dacf3fce6cdd8d06b67d329c677ef84
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-07-04 21:31:20 +0300

    Create veritipleri_degiskenler.dart

---
+++ veritipleri_degiskenler.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  int yas = 22;
+  double ortalama = 5.6;
+  num yil = 2001;
+  String ad = 'muhittin';
+  bool erkekMi = true;
+  bool issizMi = true;
+  print(ad);
+  print(yas);
+  print(yil);
+  print(ortalama);
+  print(ad);
+  print(ad + 'in en sevdigi');
+  print(ad + 'in ');
+  print(issizMi);
+  print(erkekMi);
+}
