f77a06f16fe8a4ce479aa55c2f8865e3f52a54ee
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-07-17 17:52:51 +0300

    Create static_metot.dart

---
+++ static_metot.dart
@@ -0,0 +1,33 @@
+void main(List<String> args) {
+  /*var plt = "Gökdeniz";
+  plt = "Sarıibrahim";
+
+  final String plt = "Gökdeniz";
+  plt = "Sarıibrahim";
+
+  const String ttl = "Sarıibrahim";
+  ttl = "Gökdeniz";
+  
+
+  const date = dateTime.now();
+  final date2 = DateTime.now()
+
+  final List = [1, 2, 3];
+  const List2 = [1, 2, 3];
+  
+  List.add(5);
+  List2.add(7);*/
+// canonicalized
+  const List = [1, 2];
+  const List2 = [1, 2];
+  const List3 = [1, 2];
+  const List4 = [1, 2];
+
+  /*List.add(7);
+  List.add(7);*/
+
+  if (List == List2 && List3 == List4) {
+    print("eşit");
+  } else
+    print("eşit değil");
+}
